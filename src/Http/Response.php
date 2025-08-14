cf2887fa28bcfdaad44fd5844fa7d5d06db327b1
chinayin <10531946+chinayin@users.noreply.github.com>
2025-08-14 17:41:53 +0800

    Compatible with removing ASCII control characters (0x00-0x1F, 0x7F)

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -21,7 +21,11 @@ class Response extends \GuzzleHttp\Psr7\Response
     public function getBody(): StreamInterface
     {
         $stream = parent::getBody();
-        $data = json_decode((string)$stream, true);
+
+        // 移除 ASCII 控制字符（0x00-0x1F, 0x7F），保留标准的 \n \r \t
+        $cleaned = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]/u', '', (string)$stream);
+
+        $data = json_decode($cleaned, true);
         if (JSON_ERROR_NONE === json_last_error()) {
             // 检查北森 API 的错误响应格式
             // 统一兼容 'code' 和 'Code'
@@ -36,7 +40,14 @@ class Response extends \GuzzleHttp\Psr7\Response
             }
         }
 
-        return $stream;
+        // 包回 StreamInterface
+        if (class_exists(\GuzzleHttp\Psr7\Utils::class)) {
+            // Guzzle 7+
+            return \GuzzleHttp\Psr7\Utils::streamFor($cleaned);
+        } else {
+            // Guzzle 6.x
+            return \GuzzleHttp\Psr7\stream_for($cleaned);
+        }
     }
 
     public function toArray(): array
